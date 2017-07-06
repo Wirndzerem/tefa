e65b17248254dc3b51dd62eb051a83516156d43d
cobra <fonyuy48@gmail.com>
2017-07-06 11:07:03 +0100

    static collected and debug false

--- TEFA/settings.py
+++ TEFA/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vrp@x%i=z%j@rff_6^fq1ozzyt8nv!kx*d870u_v^c2y0qoug3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'mikilimnyuy.herokuapp.com'
